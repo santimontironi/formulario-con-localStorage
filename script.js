40119ba6e0e9e86e0b6e3ec3946c3eccf333e730
santimontironi <santiimontironi@gmail.com>
2024-10-20 17:00:41 -0300

    COMMIT

---
+++ script.js
@@ -0,0 +1,53 @@
+const btnEnvio = document.getElementById("btnenviar")
+const formulario = document.querySelector("form")
+const tbody = document.querySelector("tbody")
+
+formulario.addEventListener("submit",function(event){
+    event.preventDefault()
+
+    const nombre = document.getElementById("nombre").value
+    const apellido = document.getElementById("apellido").value
+    const edad = document.getElementById("edad").value
+
+    const fila = document.createElement("tr")
+    fila.classList.add("striped")
+
+    fila.innerHTML = `
+        <td>${nombre}</td>
+        <td>${apellido}</td>
+        <td>${edad}</td>
+    `
+
+    const persona = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad
+    }
+
+    tbody.appendChild(fila)
+
+    let personas = JSON.parse(localStorage.getItem("personas")) || [] //crea el localStorage en formato JSON y lo ultimo es para retornar si o si un array en caso de que sea null y para inicializarlo como un arreglo (aunque este vacio)
+
+    personas.push(persona) //agrega el objeto persona al localStorage
+
+    localStorage.setItem("personas", JSON.stringify(personas)) //Transforma a string el JSON para guardarlo correctamente.
+
+    nombre.value = ""
+    apellido.value = ""
+    edad.value = ""
+})
+
+document.addEventListener("DOMContentLoaded",function(){
+    let personas = JSON.parse(localStorage.getItem("personas")) || [] //Lo pasa a objeto JSON nuevamente para poder mostrarlo por pantalla
+
+    personas.forEach(persona => {
+        const fila = document.createElement("tr")
+        fila.classList.add("striped")
+        fila.innerHTML = `
+            <td>${persona.nombre}</td>
+            <td>${persona.apellido}</td>
+            <td>${persona.edad}</td>
+        `
+        tbody.appendChild(fila)
+    });
+})
